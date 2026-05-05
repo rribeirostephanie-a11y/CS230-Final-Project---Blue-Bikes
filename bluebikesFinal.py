f9f5718376d766a97855d08f9934ab3fa64d9b42
rribeirostephanie-a11y <rribeirostephanie@gmail.com>
2026-05-05 15:47:39 -0400

    Update bluebikesFinal.py

--- bluebikesFinal.py
+++ bluebikesFinal.py
@@ -115,7 +115,7 @@ def main():
 
     #query number 2 
     st.header("2. Which blue bikes stations are the busiest, and where are they located?")
-    st.write ("The busiest station in 2020 and their locations. ")
+    st.write ("Use the slider to search for the busiest stations. ")
 
     top_number = st.slider("Choose how many top stations to show:", 5,25,10)
     top_stations = get_top_counts(filters_df,"start station name", top_number)
